148f1a66a2ab17e635aaa6425fef3e1f220c925a
SinketStudios <35319739+SinketStudios@users.noreply.github.com>
2018-01-11 03:47:57 +0100

    Create core.php

---
+++ core/core.php
@@ -0,0 +1,10 @@
+<?php 
+
+define('HTML_DIR','html/');
+define('APP_TITLE_INDEX','FreestyleWeb » Inicio');
+define('APP_TITLE_TRAIN','FreestyleWeb » Entrenar');
+define('APP_COPY','Copyright &copy; ' . date('Y',time()) . ' FreestyleWeb.');
+define('APP_VERSION','Develop v0.0.2');
+
+
+ ?>
